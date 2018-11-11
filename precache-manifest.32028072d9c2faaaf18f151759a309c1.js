44ea7df2393aa757287eb9e3417a862ce6ee1211
IliyaKobaliya <kobaliya0506@gmail.com>
2018-11-11 11:21:33 +0200

    DIST

---
+++ precache-manifest.32028072d9c2faaaf18f151759a309c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a56df9c980af6005c86d",
+    "url": "/static/js/main.a56df9c9.chunk.js"
+  },
+  {
+    "revision": "6ca417d2fac47c7744dd",
+    "url": "/static/js/1.6ca417d2.chunk.js"
+  },
+  {
+    "revision": "a56df9c980af6005c86d",
+    "url": "/static/css/main.bbbc1994.chunk.css"
+  },
+  {
+    "revision": "c1e00747218442c4a7d86e0c7d18f9ea",
+    "url": "/index.html"
+  }
+];
